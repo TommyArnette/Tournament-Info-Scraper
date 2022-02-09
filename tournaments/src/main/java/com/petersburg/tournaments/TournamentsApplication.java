7f9b508747f7ea0dabbd27741ecc5693705f4ecf
Tommy Arnette <tommy.arnette@gmail.com>
2022-02-08 22:25:27 -0500

    Added Selenium & JUnit dependencies to pom

--- tournaments/src/main/java/com/petersburg/tournaments/TournamentsApplication.java
+++ tournaments/src/main/java/com/petersburg/tournaments/TournamentsApplication.java
@@ -3,6 +3,7 @@ package com.petersburg.tournaments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class TournamentsApplication {
 
--- tournaments/src/test/java/com/petersburg/tournaments/TournamentsApplicationTests.java
+++ tournaments/src/test/java/com/petersburg/tournaments/TournamentsApplicationTests.java
@@ -2,12 +2,36 @@ package com.petersburg.tournaments;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
 
 @SpringBootTest
 class TournamentsApplicationTests {
+	public WebDriver driver;
 
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	public void firefoxSession() {
+		WebDriverManager.firefoxdriver().setup();
+
+		driver = new FirefoxDriver();
+
+		driver.quit();
+	}
+
+	@Test
+	public void chromeSession() {
+		WebDriverManager.chromedriver().setup();
+
+		driver = new ChromeDriver();
+
+		driver.quit();
+	}
+
 
 }
