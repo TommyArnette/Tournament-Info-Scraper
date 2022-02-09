ac46ce058044454556073d52b5ecdd97e66abd06
Tommy Arnette <tommy.arnette@gmail.com>
2022-02-09 10:58:35 -0500

    Amended implicitlyWait to accept 500ms wait time. Added basic script to open Chrome browser and do a google search for Selenium.

---
+++ tournaments/src/main/java/com/petersburg/tournaments/HelloSelenium.java
@@ -0,0 +1,32 @@
+package com.petersburg.tournaments;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class HelloSelenium {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://google.com");
+
+        driver.getTitle(); // => "Google"
+
+        driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
+
+        WebElement searchBox = driver.findElement(By.name("q"));
+        WebElement searchButton = driver.findElement(By.name("btnK"));
+
+        searchBox.sendKeys("Selenium");
+        searchButton.click();
+
+        searchBox = driver.findElement(By.name("q"));
+        searchBox.getAttribute("Selenium"); // => "Selenium"
+
+        driver.quit();
+    }
+}
