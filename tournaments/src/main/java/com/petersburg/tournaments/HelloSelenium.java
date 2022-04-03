dc9b940242d7e89d84d2ad28e9e46dc56d0a02d9
Tommy Arnette <tommy.arnette@gmail.com>
2022-04-02 23:07:49 -0400

    Significant changes. Updated script to navigate to USTA junior page. Pre-selects the Junior category. Enters a zip code and selects "Enter."

--- tournaments/src/main/java/com/petersburg/tournaments/HelloSelenium.java
+++ tournaments/src/main/java/com/petersburg/tournaments/HelloSelenium.java
@@ -2,36 +2,55 @@ package com.petersburg.tournaments;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class HelloSelenium {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver;
 
         ChromeOptions options = new ChromeOptions();
         driver = new ChromeDriver(options);
 
-        driver.get("https://selenium.dev");
+        driver.manage().window().maximize(); // Maximizes the window
 
-        driver.getTitle(); // => "Google"
+        /**
+         * Lines 29-33 opens a Chrome browser session, navigates to the USTA tournaments page
+         * with "Junior (18 and Under)" pre-selected, enters "30809" in the location text box, and hits "enter."
+         *
+         * A list of all tournaments within 100 miles of the entered zipcode appears on screen.
+         */
+            driver.get("https://playtennis.usta.com/tournaments?level-category=junior");
+            driver.getTitle(); // => "Google"
+            driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
+            WebElement searchBox = driver.findElement(By.name("location"));
+            searchBox.sendKeys("30809" + Keys.ENTER);
 
-        driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
+        /**
+         * The next section of code automates the process of selecting a tournament link,
+         * finding the tournament ID, name, date, location, and cost, and enters that information into a formatted
+         * spreadsheet.
+         *
+         * A List is created to contain the search results. An enhanced for loop automates the steps for select
+         * a tournament and retrieve data for each result.
+         */
+            driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
 
-        WebElement searchBox = driver.findElement(By.name("q"));
-        WebElement searchButton = driver.findElement(By.name("btnK"));
+            List<WebElement> tournamentSearchResults = driver.findElements(By.className("csa-search-result-tournament"));
 
-        searchBox.sendKeys("Selenium");
-        searchButton.click();
+            for (WebElement tournamentSearchResult : tournamentSearchResults) {
+                System.out.println("Got some results");
+            }
 
-        searchBox = driver.findElement(By.name("q"));
-        searchBox.getAttribute("Selenium"); // => "Selenium"
 
-        driver.quit();
+
+       // driver.quit(); //CLOSES BROWSER
     }
 }
